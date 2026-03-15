81f093cf9d1f62ff006afd53ebdc31a745f92564
El Secreto de la Brasa <elsecretodelabrasa@gmail.com>
2026-03-15 13:06:42 +0100

    feat: Implement a responsive app shell with desktop navigation, add i18n support, and introduce new global styles and assets.

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCzgDoGyagqcw7483x1jR-A-z34dNxpRig",
+    authDomain: "el-secrreto-de-la-brasa.firebaseapp.com",
+    projectId: "el-secrreto-de-la-brasa",
+    storageBucket: "el-secrreto-de-la-brasa.firebasestorage.app",
+    messagingSenderId: "52524832572",
+    appId: "1:52524832572:web:a14615c9dea5d39327b039",
+    measurementId: "G-WLGPS9BZD4"
+};
